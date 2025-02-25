b577aa918246087a3f3b953d31f645ebf90e479e
KarmicSine68 <117841894+KarmicSine68@users.noreply.github.com>
2025-02-25 14:34:34 -0600

    Grid is created and spawns a player

---
+++ Gettin Griddy/Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+/******************************************************************************
+ * Author: Brad Dixon
+ * File Name: GameManager.cs
+ * Creation Date: 2/25/2025
+ * Brief: Keeps track of the status of the grid and where everything is
+ * ***************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject gridSpace;
+
+    GameObject[,] grid;
+
+    [Header("Grid Parameters")]
+    [Tooltip("How many rows are in the grid")]
+    [SerializeField] private int rows;
+    [Tooltip("How many columns are in the grid")]
+    [SerializeField] private int columns;
+
+    /// <summary>
+    /// Creates the grid and spwans things in
+    /// </summary>
+    private void Start()
+    {
+        grid = new GameObject[columns, rows];
+        for(int i = 0; i < rows; ++i)
+        {
+            for(int j = 0; j < columns; ++j)
+            {
+                GameObject tile = Instantiate(gridSpace, new Vector3(j, 0, i), Quaternion.identity);
+                grid[j, i] = tile;
+            }
+        }
+
+        SpawnPlayer();
+    }
+
+    private void SpawnPlayer()
+    {
+        int x = Random.Range(0, columns);
+        int y = Random.Range(0, rows);
+
+        Vector3 spawnPos = grid[x, y].transform.position;
+
+        spawnPos += new Vector3(.5f, 1.5f, .5f);
+
+        Instantiate(player, spawnPos, Quaternion.identity);
+    }
+}
