59d503853d97a9e6c742ebe65133adc6ea830405
TylerBouchard <117851242+TylerBouchard@users.noreply.github.com>
2025-02-27 17:02:43 -0600

    attack
    attacking working except when its out of bounds

--- Gettin Griddy/Assets/Scripts/GameManager.cs
+++ Gettin Griddy/Assets/Scripts/GameManager.cs
@@ -12,7 +12,8 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject player;
     [SerializeField] private GameObject gridSpace;
-    [SerializeField] private GameObject playerTracker;
+    [SerializeField] public TileBehaviour playerTile;
+    [SerializeField] private List<TileBehaviour> EnemyTiles;
     [SerializeField] private GameObject boulder;
 
     GameObject[,] grid;
@@ -35,9 +36,9 @@ public class GameManager : MonoBehaviour
             {
                 GameObject tile = Instantiate(gridSpace, new Vector3(j, 0, i), Quaternion.identity);
                 grid[j, i] = tile;
+                tile.GetComponent<TileBehaviour>().gridLocation = new Vector2(j, i);
             }
         }
-
         SpawnPlayer();
         SpawnBoulder();
     }
@@ -61,22 +62,31 @@ public class GameManager : MonoBehaviour
     /// Updates where in the grid the player is
     /// </summary>
     /// <param name="tile"></param>
-    public void TrackPlayer(GameObject tile)
+    public void TrackPlayer(TileBehaviour tileScript)
     {
-        playerTracker = tile;
-        
-        for(int i = 0; i < rows; ++i)
+        playerTile = tileScript;
+    }
+    public void TrackEnemy(TileBehaviour tileScript)
+    {
+        EnemyTiles.Add(tileScript);
+    }
+    public void Attack(Vector2 attackDir) {
+        List<TileBehaviour> tilesToAttack = new List<TileBehaviour>();
+        Vector2 playerPos = playerTile.gridLocation;
+        tilesToAttack.Add(grid[(int)(playerPos.x + attackDir.x), (int)(playerPos.y + attackDir.y)].GetComponent<TileBehaviour>());
+        if (attackDir.x != 0)
         {
-            for(int j = 0; j < columns; ++j)
-            {
-                if(grid[j,i] == playerTracker)
-                {
-                    grid[j, i] = playerTracker;
-                }
-            }
+            tilesToAttack.Add(grid[(int)(playerPos.x + attackDir.x), (int)(playerPos.y + attackDir.y + 1)].GetComponent<TileBehaviour>());
+            tilesToAttack.Add(grid[(int)(playerPos.x + attackDir.x), (int)(playerPos.y + attackDir.y - 1)].GetComponent<TileBehaviour>());
+        }
+        else {
+            tilesToAttack.Add(grid[(int)(playerPos.x + attackDir.x + 1), (int)(playerPos.y + attackDir.y)].GetComponent<TileBehaviour>());
+            tilesToAttack.Add(grid[(int)(playerPos.x + attackDir.x - 1), (int)(playerPos.y + attackDir.y)].GetComponent<TileBehaviour>());
+        }
+        foreach (TileBehaviour t in tilesToAttack) {
+            t.Jiggle();
         }
     }
-
     /// <summary>
     /// Spawns a boulder that obstructs the player's movement
     /// </summary>
@@ -89,7 +99,7 @@ public class GameManager : MonoBehaviour
 
         spawnPos += new Vector3(.5f, 1.5f, .5f);
 
-        if (grid[x, y] == playerTracker)
+        if (grid[x, y] == playerTile)
         {
             SpawnBoulder();
         }
--- Gettin Griddy/Assets/Scripts/PlayerBehaviour.cs
+++ Gettin Griddy/Assets/Scripts/PlayerBehaviour.cs
@@ -4,6 +4,7 @@
  * Creation Date 2/25/2025
  * Brief: Controls actions for the player
  * ***************************************************************************/
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,7 +14,11 @@ public class PlayerBehaviour : GridMovement
 {
     InputActionMap playerMap;
     InputAction playerMove;
+    InputAction playerAttack;
+    InputAction playerAttackToggle;
 
+    private GameManager gm;
+    public bool attacking = false;
     bool canMove = true;
 
     /// <summary>
@@ -21,12 +26,30 @@ public class PlayerBehaviour : GridMovement
     /// </summary>
     private void Awake()
     {
+        gm = FindObjectOfType<GameManager>();
         playerMap = GetComponent<PlayerInput>().currentActionMap;
         playerMap.Enable();
 
         playerMove = playerMap.FindAction("Movement");
+        playerAttack = playerMap.FindAction("Attack"); ;
+        playerAttackToggle = playerMap.FindAction("AttackToggle"); ;
 
         playerMove.started += PlayerMove_started;
+        playerAttack.started += PlayerAttack;
+        playerAttackToggle.started += ToggleAttackMode;
+    }
+
+    private void PlayerAttack(InputAction.CallbackContext obj)
+    {
+        if (attacking) {
+            Vector2 attackDir = playerMove.ReadValue<Vector2>();
+            gm.Attack(attackDir);
+        }
+    }
+
+    private void ToggleAttackMode(InputAction.CallbackContext obj)
+    {
+        attacking = !attacking;
     }
 
     /// <summary>
@@ -35,31 +58,35 @@ public class PlayerBehaviour : GridMovement
     /// <param name="obj"></param>
     private void PlayerMove_started(InputAction.CallbackContext obj)
     {
-        canMove = false;
-        Vector2 moveDir = playerMove.ReadValue<Vector2>();
+        if (!attacking) {
+            canMove = false;
+            Vector2 moveDir = playerMove.ReadValue<Vector2>();
 
-        if(moveDir.x > 0)
-        {
-            MovePlayer(Vector3.right, "Right");
-        }
-        else if(moveDir.x < 0)
-        {
-            MovePlayer(Vector3.left, "Left");
-        }
-        else if(moveDir.y > 0)
-        {
-            MovePlayer(Vector3.forward, "Up");
-        }
-        else if(moveDir.y < 0)
-        {
-            MovePlayer(Vector3.back, "Down");
+            if (moveDir.x > 0)
+            {
+                MovePlayer(Vector3.right, "Right");
+            }
+            else if (moveDir.x < 0)
+            {
+                MovePlayer(Vector3.left, "Left");
+            }
+            else if (moveDir.y > 0)
+            {
+                MovePlayer(Vector3.forward, "Up");
+            }
+            else if (moveDir.y < 0)
+            {
+                MovePlayer(Vector3.back, "Down");
+            }
         }
     }
 
     private void OnDisable()
     {
         playerMap.Disable();
-
+        
         playerMove.started -= PlayerMove_started;
+        playerAttack.started -= ToggleAttackMode;
+        playerAttackToggle.started -= PlayerAttack;
     }
 }
