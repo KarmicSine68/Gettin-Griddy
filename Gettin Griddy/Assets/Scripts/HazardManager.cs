38d12693390edf33cf768183ff6caee957fe3c86
BurnoutBeat <jakub.gorski5@gmail.com>
2025-03-06 16:35:46 -0600

    Establishing Hazard Manager

--- Gettin Griddy/Assets/Scripts/HazardManager.cs
+++ Gettin Griddy/Assets/Scripts/HazardManager.cs
@@ -19,20 +19,54 @@ public class HazardManager : MonoBehaviour
         
     }
 
+
     private void SpawnCross()
     {
-        for
-        for (int i = 0; i < 8; i++)
+        int randOne = Random.Range(0, 8);
+        int randTwo = Random.Range(0, 8);
+        if(randOne == randTwo)
         {
-            Vector3 spawnPos = gameManager.grid[row, i].transform.position;
-            spawnPos += new Vector3(.5f, 1.5f, .5f);
-            gameManager.grid[row, i].objectOnTile = Instantiate(hazard, spawnPos, Quaternion.identity);
+            if(randTwo == 0)
+            {
+                randTwo = randTwo + 1;
+            }
+            if(randTwo == 8)
+            {
+                randTwo = randTwo - 1;
+            }
         }
-        for (int i = 0; i < 8; i++)
+
+        int randChoice = Random.Range(0, 1);
+        if(randChoice == 0)//spawns on rows
         {
-            Vector3 spawnPos = gameManager.grid[i, col].transform.position;
-            spawnPos += new Vector3(.5f, 1.5f, .5f);
-            gameManager.grid[i, col].objectOnTile = Instantiate(hazard, spawnPos, Quaternion.identity);
+            for (int i = 0; i < 8; i++)
+            {
+                Vector3 spawnPos = gameManager.grid[randOne, i].transform.position;
+                spawnPos += new Vector3(.5f, 1.5f, .5f);
+                gameManager.grid[randOne, i].objectOnTile = Instantiate(hazard, spawnPos, Quaternion.identity);
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                Vector3 spawnPos = gameManager.grid[randTwo, i].transform.position;
+                spawnPos += new Vector3(.5f, 1.5f, .5f);
+                gameManager.grid[randTwo, i].objectOnTile = Instantiate(hazard, spawnPos, Quaternion.identity);
+            }
         }
+        if(randChoice == 1)//spawn on columns
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                Vector3 spawnPos = gameManager.grid[i, randOne].transform.position;
+                spawnPos += new Vector3(.5f, 1.5f, .5f);
+                gameManager.grid[i, randOne].objectOnTile = Instantiate(hazard, spawnPos, Quaternion.identity);
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                Vector3 spawnPos = gameManager.grid[i, randTwo].transform.position;
+                spawnPos += new Vector3(.5f, 1.5f, .5f);
+                gameManager.grid[i, randTwo].objectOnTile = Instantiate(hazard, spawnPos, Quaternion.identity);
+            }
+        }
+
     }
 }
