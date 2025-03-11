002fa879b3719368c9ae499d8f487c91688ccdb8
hhlaxbro <Afederspiel331@gmail.com>
2025-03-11 16:30:27 -0500

    skybox
    needs to be rotated

---
+++ Gettin Griddy/Assets/Scripts/Skybox.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Skybox : MonoBehaviour
+{
+    public float x;
+    public float y;
+    public float z;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetSkyBoxRotation();
+    }
+
+    public Vector3 SkyBoxRotation;
+    public void SetSkyBoxRotation(Vector3 rotation)
+    {
+        this.SkyBoxRotation = rotation;
+    }
+}
